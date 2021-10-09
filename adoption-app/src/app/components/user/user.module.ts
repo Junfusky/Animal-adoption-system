6f2449afa2ffc146d137c6e84d0be475106ce541
Junfusky <mxmxjun@gmail.com>
2021-10-09 11:54:43 -0400

    Config routings

--- adoption-app/src/app/components/user/user.module.ts
+++ adoption-app/src/app/components/user/user.module.ts
@@ -2,16 +2,45 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ManageAnimalComponent } from './admin-dashboard/manage-animal/manage-animal.component';
+import { ManageUserComponent } from './admin-dashboard/manage-user/manage-user.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
+const routes: Routes = [
+  { 
+    path: 'user-dashboard', component: UserDashboardComponent
+  },
+  { 
+    path: 'admin-dashboard', component: AdminDashboardComponent,
+    children: [
+      { path: '', redirectTo: 'manage-animals', pathMatch: 'full' },
+      { path: 'manage-animals', component: ManageAnimalComponent },
+      { path: 'manage-users', component: ManageUserComponent }
+    ]
+  }
+];
 
 
 @NgModule({
   declarations: [
     UserDashboardComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    ManageAnimalComponent,
+    ManageUserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    UserDashboardComponent,
+    AdminDashboardComponent,
+    ManageAnimalComponent,
+    ManageUserComponent
   ]
 })
 export class UserModule { }

713e25ab82bc126d90e5d0c77f3b814e119729e1
Junfusky <mxmxjun@gmail.com>
2021-10-11 11:28:41 -0400

    Change put method to patch

--- adoption-app/backend/routes/animals.js
+++ adoption-app/backend/routes/animals.js
@@ -36,19 +36,65 @@ router.post('/', [auth, admin], async (req, res) => {
     res.send(animal);
 })
 
-// update an animal
-router.put('/:id', [auth, admin], async (req, res) => {
-    delete req.body._id;
-    const { error } = validateAnimal(req.body);
-    if(error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    const animal = await Animal.findByIdAndUpdate(req.params.id, { status: req.body.status}, {new:true});
+// // update an animal
+// router.put('/:id', [auth, admin], async (req, res) => {
+//     delete req.body._id;
+//     const { error } = validateAnimal(req.body);
+//     if(error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
+//     const animal = await Animal.findByIdAndUpdate(req.params.id, { status: req.body.status}, {new:true});
 
-    if(!animal) {
+//     if(!animal) {
+//         return res.status(404).send('The animal with given id does not exist.');
+//     }
+//     res.send(animal);
+// })
+
+// update animal info
+router.patch('/:id', [auth, admin], async (req, res) => {
+    if(!req.params.id) {
         return res.status(404).send('The animal with given id does not exist.');
     }
-    res.send(animal);
+
+    // build updated info
+    if(req.body.name) {
+        await Animal.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new:true});
+    }
+    if(req.body.sex) {
+        await Animal.findByIdAndUpdate(req.params.id, {sex: req.body.sex}, {new:true});
+    }
+    if(req.body.breed) {
+        await Animal.findByIdAndUpdate(req.params.id, {breed: req.body.breed}, {new:true});
+    }
+    if(req.body.species) {
+        await Animal.findByIdAndUpdate(req.params.id, {species: req.body.species}, {new:true});
+    }
+    if(req.body.shelter) {
+        await Animal.findByIdAndUpdate(req.params.id, {shelter: req.body.shelter}, {new:true});
+    }
+    if(req.body.state) {
+        await Animal.findByIdAndUpdate(req.params.id, {state: req.body.state}, {new:true});
+    }
+    if(req.body.city) {
+        await Animal.findByIdAndUpdate(req.params.id, {city: req.body.city}, {new:true});
+    }
+    if(req.body.zip) {
+        await Animal.findByIdAndUpdate(req.params.id, {zip: req.body.zip}, {new:true});
+    }
+    if(req.body.DoB) {
+        await Animal.findByIdAndUpdate(req.params.id, {DoB: req.body.DoB}, {new:true});
+    }
+    if(req.body.pic) {
+        await Animal.findByIdAndUpdate(req.params.id, {pic: req.body.pic}, {new:true});
+    }
+    if(req.body.requestList) {
+        await Animal.findByIdAndUpdate(req.params.id, {requestList: req.body.requestList}, {new:true});
+    }
+    if(req.body.status) {
+        await Animal.findByIdAndUpdate(req.params.id, {status: req.body.status}, {new:true});
+    }
+    res.send('Successfuly updated!');
 })
 
 // delete an animal

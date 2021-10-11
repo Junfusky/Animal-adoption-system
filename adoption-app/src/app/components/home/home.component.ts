1c50577e3802e735f8bc55c6e675d690c574b84d
Junfusky <mxmxjun@gmail.com>
2021-10-11 12:39:16 -0400

    Create and build home component

--- adoption-app/src/app/components/home/home.component.ts
+++ adoption-app/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faDog, faCat} from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { DataService } from './../../shared/service/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faDog = faDog;
+  faCat = faCat;
+  searchZip: string = '';
+  newsList: {img: String, des: String, src: String}[] = [
+    {img: '', des: '', src: ''}
+  ];
 
-  constructor() { }
+  constructor(private router: Router,
+              private _dataService: DataService
+    ) { }
 
   ngOnInit(): void {
+    this.newsList = this._dataService.homepageNews;
+  }
+
+  searchByZip() {
+    const zip = this.searchZip;
+    this.router.navigate(['animal-list'], {queryParams:{zip}});
+  }
+
+  showDogList() {
+    const species = 'dog';
+    this.router.navigate(['animal-list'], {queryParams:{species}});
+  }
+
+  showCatList() {
+    const species = 'cat'
+    this.router.navigate(['animal-list'], {queryParams:{species}});
   }
 
 }
